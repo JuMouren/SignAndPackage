9c80dc6ce2808addc2e38cd864325d4e8c3df1fd
Travis Evashkevich <tevashkevich@gmail.com>
2017-06-17 14:25:55 +0100

    Made run.bat interactable.

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
-java -jar ./ucf.jar -package -storetype  PKCS12  -keystore  %1  -storepass %2  %3.zxp  -C  ./%3 .
+set /p folder=Folder With Ext Files:
+set /p zxpName=Desired ZXPName:
+set /p keystorefile=FileName of keystore:
+set /p keypass=Keystore Password:
+
+
+java -jar ./ucf.jar -package -storetype  PKCS12  -keystore  %keystorefile% -storepass %keypass% -tsa http://time.certum.pl/ %zxpName%.zxp  -C  %folder% .
+ 
+pause
